537743ac12c8bd9ae49db709118a7d52aada1dc4
JosephAyo <josephayo110@gmail.com>
2020-07-08 03:10:20 +0100

    added featured items cards

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -8,11 +8,21 @@ import {
     Button,
     Container,
     Tabs,
-    Tab
+    Tab,
+    OverlayTrigger,
+    Tooltip
 } from 'react-bootstrap';
+import { 
+    Star,
+    ShoppingCartTwoTone,
+    FavoriteTwoTone,
+    VisibilityTwoTone,
+    CompareArrows
+} from '@material-ui/icons';
 import NavUp from './Components/NavUp';
-
+import FeaturedProds from './Data/Feautured Prods.json';
 const Home = ()=>{
+    const [highlight,setHighlight] = React.useState();
     return(
         <Container className="home-body">
             <Row className="justify-content-center" id="large-home-banner">
@@ -47,78 +57,83 @@ const Home = ()=>{
                         <option value="mercedes">Mercedes sadasdkad adjkas</option>
                         <option value="audi">Audi</option>
                         </select>
-                            <p>
-                            featured
-                            </p>
-                            <p>
-                            featured
-                            </p>
-                            <p>
-                            featured
-                            </p>
-                            <p>
-                            featured
-                            </p>
-                            <p>
-                            featured
-                            </p>
-                            <p>
-                            featured
-                            </p>
-                            <p>
-                            featured
-                            </p>
-                            <p>
-                            featured
-                            </p>
-                            <p>
-                            featured
-                            </p>
-                            <p>
-                            featured
-                            </p>
-                            <p>
-                            featured
-                            </p>
-                            <p>
-                            featured
-                            </p>
-                            <p>
-                            featured
-                            </p>
-                            <p>
-                            featured
-                            </p>
-                            <p>
-                            featured
-                            </p>
-                            <p>
-                            featured
-                            </p>
-                            <p>
-                            featured
-                            </p>
-                            <p>
-                            featured
-                            </p>
-                            <p>
-                            featured
-                            </p>
-                            <p>
-                            featured
-                            </p>
-                            <p>
-                            featured
-                            </p>
-                            <p>
-                            featured
-                            </p>
-                            <p>
-                            featured
-                            </p>
-                            <p>
-                            featured
-                            </p>
+                            <Row className="justify-content-center">
+                            {   FeaturedProds.map((prod,index)=>{
+                                return(
+                                    <Col xs={12} sm={6} lg={3}  key={prod._id}>
+                                        <div className="item-box" 
+                                        onMouseEnter={()=>setHighlight(index)}
+                                        onTouchStart={()=>setHighlight(index)} >
+                                            <span className="product-img-span">
+                                                <img src={prod.picture} alt={prod.name}/>
+                                                <div id={highlight===index?"show-options":"hide-options"}>
+                                                <OverlayTrigger
+                                                    placement='top'
+                                                    overlay={
+                                                    <Tooltip id='tooltip-top tooltip-custom'>
+                                                        Add to cart
+                                                    </Tooltip>
+                                                    }
+                                                >
+                                                    <p>
+                                                        <ShoppingCartTwoTone/>
+                                                    </p>
+                                                </OverlayTrigger>
+                                                <OverlayTrigger
+                                                    placement='top'
+                                                    overlay={
+                                                    <Tooltip id='tooltip-top tooltip-custom'>
+                                                        Quick view
+                                                    </Tooltip>
+                                                    }
+                                                >
+                                                    <p>
+                                                        <VisibilityTwoTone/>
+                                                    </p>
+                                                </OverlayTrigger>
+                                                <OverlayTrigger
+                                                    placement='top'
+                                                    overlay={
+                                                    <Tooltip id='tooltip-top'>
+                                                        Add to favorites
+                                                    </Tooltip>
+                                                    }
+                                                >
+                                                    <p>
+                                                        <FavoriteTwoTone/>
+                                                    </p>
+                                                </OverlayTrigger>
+                                                <OverlayTrigger
+                                                    placement='top'
+                                                    overlay={
+                                                    <Tooltip id='tooltip-top tooltip-custom'>
+                                                        Compare
+                                                    </Tooltip>
+                                                    }
+                                                >
+                                                    <p>
+                                                        <CompareArrows/>
+                                                    </p>
+                                                </OverlayTrigger>
+                                                </div>
+                                            </span>
+                                            <p>
+                                                {prod.name}
+                                            </p>
+                                            <p>
+                                                {prod.description}
+                                            </p>
+                                            <p>
+                                                <Star fontSize="small"/> {prod.rating}
+                                            </p>
+                                            <p>
+                                                {prod.cost}
+                                            </p>
+                                        </div>
+                                    </Col>);
+                                }
+                            )}
+                            </Row>
                         </div>
                     </Tab>
                     <Tab eventKey="account" title="ACCOUNT">
