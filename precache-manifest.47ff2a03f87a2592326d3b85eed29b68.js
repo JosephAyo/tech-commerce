866aae34868b411c424b6f1a07388a0e6421ff6f
JosephAyo <josephayo110@gmail.com>
2020-07-11 18:28:27 +0100

    Updates

---
+++ precache-manifest.47ff2a03f87a2592326d3b85eed29b68.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f93c451e5c5ba5d75811d8a2e6fe0b6f",
+    "url": "/tech-commerce/index.html"
+  },
+  {
+    "revision": "bfeb8c9dcb1a32f1156e",
+    "url": "/tech-commerce/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "58839bcdc17d10d07486",
+    "url": "/tech-commerce/static/css/main.549816c5.chunk.css"
+  },
+  {
+    "revision": "bfeb8c9dcb1a32f1156e",
+    "url": "/tech-commerce/static/js/2.96dc33bd.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/tech-commerce/static/js/2.96dc33bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58839bcdc17d10d07486",
+    "url": "/tech-commerce/static/js/main.71c0fb3c.chunk.js"
+  },
+  {
+    "revision": "1199a30fa2f1a56f1bab",
+    "url": "/tech-commerce/static/js/runtime-main.4ecf4850.js"
+  },
+  {
+    "revision": "9aaa3e77b8fce5e1e6c09ef944064203",
+    "url": "/tech-commerce/static/media/halfwave.9aaa3e77.svg"
+  }
+]);
