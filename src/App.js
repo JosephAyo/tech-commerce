456a105379a3cd46652f258913a469bb6ed1d7b4
JosephAyo <josephayo110@gmail.com>
2020-07-11 18:42:03 +0100

    changed to hasrouter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Pages/style/App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route
 } from "react-router-dom";
