6ae88ab69421db35ef5f147d65399a9709ccab0e
JosephAyo <josephayo110@gmail.com>
2020-07-11 18:45:04 +0100

    Updates

--- precache-manifest.ea2378d09b42d96b1cff115b5f18351c.js
+++ precache-manifest.ea2378d09b42d96b1cff115b5f18351c.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f93c451e5c5ba5d75811d8a2e6fe0b6f",
+    "revision": "9dcb1baf140396a15dfce47174ff161e",
     "url": "/tech-commerce/index.html"
   },
   {
-    "revision": "bfeb8c9dcb1a32f1156e",
+    "revision": "5b4fa834a626f84517c7",
     "url": "/tech-commerce/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "58839bcdc17d10d07486",
+    "revision": "8989a92ba99c4af0ae7a",
     "url": "/tech-commerce/static/css/main.549816c5.chunk.css"
   },
   {
-    "revision": "bfeb8c9dcb1a32f1156e",
-    "url": "/tech-commerce/static/js/2.96dc33bd.chunk.js"
+    "revision": "5b4fa834a626f84517c7",
+    "url": "/tech-commerce/static/js/2.e1278232.chunk.js"
   },
   {
     "revision": "5e9b6f5c16b720e8a86875f97734673e",
-    "url": "/tech-commerce/static/js/2.96dc33bd.chunk.js.LICENSE.txt"
+    "url": "/tech-commerce/static/js/2.e1278232.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "58839bcdc17d10d07486",
-    "url": "/tech-commerce/static/js/main.71c0fb3c.chunk.js"
+    "revision": "8989a92ba99c4af0ae7a",
+    "url": "/tech-commerce/static/js/main.bf94460c.chunk.js"
   },
   {
     "revision": "1199a30fa2f1a56f1bab",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tech-commerce/precache-manifest.47ff2a03f87a2592326d3b85eed29b68.js"
+  "/tech-commerce/precache-manifest.ea2378d09b42d96b1cff115b5f18351c.js"
 );
 
 self.addEventListener('message', (event) => {
